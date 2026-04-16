f6d975497cc63e3192eab3d01fb74e97ef612c54
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-04-16 15:37:57 +0900

    Remove -llog from Android.mk
    Remove log library dependency from shared library.

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -3,5 +3,4 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE           := proc
 LOCAL_SRC_FILES        := proc.c
-LOCAL_LDLIBS           := -llog
 include $(BUILD_SHARED_LIBRARY)
